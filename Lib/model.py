89566664d4762d21ce7c160da80faa1137d8fc84
Kakise <me@kakise.xyz>
2018-05-14 15:00:29 +0000

    Ajout de nouvelle classe, reprise du code à 0

---
+++ Lib/model.py
@@ -0,0 +1,62 @@
+# Name: LaSimulation
+# Desc: Simulation du traffic autoroutier interactive
+# Repo: https://github.com/Kakise/LaSimulation
+# Author: Kakise
+# License: GPL-3.0
+# Path: -/Lib/model.py
+
+# Imports
+import numpy as np
+import random as rd
+import os as os
+
+rd.seed(os.urandom(1024))
+
+# Modélisation générale
+# Classes utilsées directement dans le calcul et la modélisation du projet
+
+# Intelligent Driver Model
+#
+# @param v0 = desired speed
+# @param T = time headway -> ~1.8s is a great value
+# @param s0 = minimum gap -> 78 meters in France
+# @param a = maximum acceleration
+# @param b = confortable deceleration
+
+class IDM:
+    def __init__(self, v0, T, s0, a, b):
+        self.v0 = v0
+        self.T = T
+        self.s0 = s0
+        self.a = a
+        self.b = b
+        
+        # Consts
+        self.alpha_v0=1
+        self.speedlimit=1000
+        self.speedmax=1000
+        self.bmax=18
+
+    # Acceleration function
+    # @param s:     actual gap [m]
+    # @param v:     actual speed [m/s]
+    # @param vl:    leading speed [m/s]
+    # @param al:    leading acceleration [m/s^2] (optional; al=0 if 3 args)
+
+    def acceleration(self, s, v, vl, al):
+        if s<0.0001:
+            return -self.bmax
+        
+        noiseAcc = 0.3
+        accRnd = noiseAcc*(rd.randrange(0,1)-0.5)
+
+        v0eff = min(self.v0, self.speedlimit, self.speedmax)
+        v0eff *= self.alpha_v0
+
+        if v < v0eff:
+            accFree = self.a*(1-np.power(v/v0eff,4))
+        else:
+            accFree = self.a*(1-v/v0eff)
+
+        sstar = self.s0 + max(0, v*self.T+0.5*v*(v-vl)/np.sqrt(self.a*self.b))
+        accInt = -self.a*np.power(sstar/np.max(s,self.s0),2)
--- main.py
+++ main.py
@@ -3,24 +3,8 @@
 # Repo: https://github.com/Kakise/LaSimulation
 # Author: Kakise
 # License: GPL-3.0
-# Project: -
+# Path: -
 
-import numpy as np
-import random as rd
-import curses
-import Lib.car
-import Lib.misc as misc
-import Lib.gui as gui
+import Lib.model as mdl
 
-stdscr = curses.initscr()
-
-# Definition des paramètres principaux.
-
-Vehicles = [] # Liste des véhicules existants
-Types = ["Car", "Truck"] # Types de véhicules existants
-StartX, StartY = (0, 0) # Position de départ
-NVeh = 1200 # Nombre de véhicules
-Speed_m, Speed_M = (110, 130) # Vitesse minimale et vitesse maximale (resp.)
-
-gui.main(stdscr)
-misc.cls()
+Model = mdl.IDM(130, 1.8, 78, 2, 2)
