fd766106ea04afe8f87ea780d488e9db1dcdf966
Kakise <me@kakise.xyz>
2018-05-22 21:50:12 +0000

    Code cleaning

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ dt=0.01
 end=40
 
 def simulation(t,dt,end):
-    CarArr=[mdl.Vehicle(1, 1, 100, 0, 130/3.6, "car"), mdl.Vehicle(1, 1, 200, 0, 130/3.6, "car")]
+    CarArr = [mdl.Vehicle(1, 1, 100, 0, 130/3.6, "car"), mdl.Vehicle(1, 1, 200, 0, 130/3.6, "car")]
     while t<end:
         t+=dt
         if rd.random() < 1/20:
-            CarArr=[mdl.Vehicle(1, 1, 0, 0, 130/3.6, "car")] + CarArr
+            CarArr = [mdl.Vehicle(1, 1, 0, 0, 130/3.6, "car")] + CarArr
         for i in range(len(CarArr)-1):
-            CarArr[i].acc = CarModel.acceleration(CarArr[i+1].u-CarArr[i].u, CarArr[i].speed, CarArr[i+1].speed, CarArr[i+1].acc)
+            CarArr[i].acc   = CarModel.acceleration(CarArr[i+1].u-CarArr[i].u, CarArr[i].speed, CarArr[i+1].speed, CarArr[i+1].acc)
             CarArr[i].speed = CarArr[i].speed + CarArr[i].acc * dt
-            CarArr[i].u = CarArr[i].u + CarArr[i].speed * dt + 1/2 * CarArr[i].acc * (dt**2)
+            CarArr[i].u     = CarArr[i].u + CarArr[i].speed * dt + 1/2 * CarArr[i].acc * (dt**2)
         # Handle last car separately
-        CarArr[len(CarArr)-1].acc = CarModel.acceleration(10**10, CarArr[i].speed, 10**10, 10**10)
+        CarArr[len(CarArr)-1].acc   = CarModel.acceleration(10**10, CarArr[i].speed, 10**10, 10**10)
         CarArr[len(CarArr)-1].speed = CarArr[i].speed + CarArr[i].acc * dt
-        CarArr[len(CarArr)-1].u = CarArr[i].u + CarArr[i].speed * dt + 1/2 * CarArr[i].acc * (dt**2)
+        CarArr[len(CarArr)-1].u     = CarArr[i].u + CarArr[i].speed * dt + 1/2 * CarArr[i].acc * (dt**2)
     for car in CarArr:
         print (car.u)
 
